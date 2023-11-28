72a32a8d4089751fbd0ff2c88b1f9673b44a3678
vinalal <121111797+vinalal@users.noreply.github.com>
2023-11-28 16:40:08 +0530

    Update market.cpp

--- market.cpp
+++ market.cpp
@@ -25,13 +25,45 @@ struct order{
 
 struct structure{
     std::map<std::string,int> quants;
+
+    bool operator==(const structure& other) const {
+        return quants==other.quants;
+    }
+    bool operator<(const structure& other) const {
+        // Customize the comparison logic based on your requirements
+        int thisTotal = 0;
+        int otherTotal = 0;
+
+        for (const auto& entry : quants) {
+            thisTotal += entry.second;
+        }
+
+        for (const auto& entry : other.quants) {
+            otherTotal += entry.second;
+        }
+
+        return thisTotal < otherTotal;
+    }
 };
 
+
 bool max_compare(const std::pair<int, order*>& lhs, const std::pair<int, order*>& rhs) {
-        return lhs.first < rhs.first; // Comparing based on the first parameter (integer)
+    if(lhs.first == rhs.first){
+        if(lhs.second->start_time == rhs.second->start_time){
+            return lhs.second->broker > rhs.second->broker;
+        }
+        return lhs.second->start_time > rhs.second->start_time;
+    }
+    return lhs.first < rhs.first; // Comparing based on the first parameter (integer)
 };
 
 bool min_compare(const std::pair<int, order*>& lhs, const std::pair<int, order*>& rhs) {
+    if(lhs.first == rhs.first){
+        if(lhs.second->start_time == rhs.second->start_time){
+            return lhs.second->broker > rhs.second->broker;
+        }
+        return lhs.second->start_time > rhs.second->start_time;
+    }
     return lhs.first > rhs.first; // Comparing based on the first parameter (integer)
 };
 
@@ -66,7 +98,7 @@ void market::start()
     inputFile.close();
     
     std::string word = "";
-    int a = 0;
+    a = 0;
     std::vector<order*> trade_orders;
     for(int i=0;i<orders.size();i++){
         word = "";
@@ -95,7 +127,7 @@ void market::start()
                             t->end_time = t->start_time + std::stoi(word);
                         }
                         else{
-                            t->end_time = INT64_MAX;
+                            t->end_time = 10000;
                         }
                     }
                     if(word.substr(0,1) == "$"){
@@ -126,160 +158,204 @@ void market::start()
     // Now, i will start iterating the orders
     
 
-    std::map<structure*,std::priority_queue<std::pair<int, order*>, std::vector<std::pair<int, order*>>, decltype(&max_compare)>> buy_queues;   // max heap for buy
+    std::map<structure,std::priority_queue<std::pair<int, order*>, std::vector<std::pair<int, order*>>, decltype(&max_compare)>> buy_queues;   // max heap for buy
     std::pair<int,order*> x;
     std::vector<std::pair<int,order*>> v;
-    std::map<structure*,std::priority_queue<std::pair<int, order*>, std::vector<std::pair<int, order*>>, decltype(&min_compare)>> sell_queues;  // min heap for sell
+    std::map<structure,std::priority_queue<std::pair<int, order*>, std::vector<std::pair<int, order*>>, decltype(&min_compare)>> sell_queues;  // min heap for sell
     for(int i=0;i<trade_orders.size();i++){
         struct order* t = trade_orders[i];
-        struct structure* s = new structure;
-        s->quants = t->quantities;
+        struct structure s;
+        s.quants = t->quantities;
         if(t->offer == 0){  // buy order
             if(sell_queues[s].empty()){
                 buy_queues[s].push(std::make_pair(t->price,t));
                 continue;
             }
             else{
-                if(t->price < sell_queues[s].top().first){
-                    buy_queues[s].push(std::make_pair(t->price,t));
-                    continue;
-                }
-                if(t->price >= sell_queues[s].top().first){
-                    while(!sell_queues[s].empty() && ((t->start_time > sell_queues[s].top().second->end_time /*&& t->end_time > sell_queues[s].top().second->end_time*/) || (/*t->start_time < sell_queues[s].top().second->start_time &&*/ t->end_time < sell_queues[s].top().second->start_time))){
-                        // iterate through the sell_queues till we get a valid order
-                        v.push_back(sell_queues[s].top());
-                        sell_queues[s].pop();
+                while(!sell_queues[s].empty()){
+                    if(t->price < sell_queues[s].top().first){
+                        break;
                     }
-                    if(sell_queues[s].empty()){  // all orders are invalid with current order
-                        while(!v.empty()){
-                            sell_queues[s].push(v.back());
-                            v.pop_back();
-                        }
-                        buy_queues[s].push(std::make_pair(t->price,t));
-                        continue;
-                    }
-                    int p = sell_queues[s].top().first;
-                    x = sell_queues[s].top();
-                    int min_time = sell_queues[s].top().second->start_time;
-                    while(!sell_queues[s].empty() && sell_queues[s].top().first == p ){
+                    else if(t->price >= sell_queues[s].top().first){
                         if(t->start_time > sell_queues[s].top().second->end_time){
                             v.push_back(sell_queues[s].top());
                             sell_queues[s].pop();
                             continue;
                         }
-                        if(min_time > sell_queues[s].top().second->start_time){
-                            min_time = sell_queues[s].top().second->start_time;
-                            x = sell_queues[s].top();
-                            sell_queues[s].pop();
-                            v.push_back(x);
-                        }
-                        else if(min_time == sell_queues[s].top().second->start_time){
-                            if(x.second->broker > sell_queues[s].top().second->broker){
-                                x = sell_queues[s].top();
+                        // if it reaches here then it means we have found the deal for the best trade...now take it and go to next one if quantity still remains
+                        x = sell_queues[s].top();
+                        if(t->quantity < x.second->quantity){
+                            x.second->quantity -= t->quantity;
+                            //now print the message
+                            std::cout<<t->broker<<" purchased "<<t->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
                             }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << x.second->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            break;
+                        }
+                        else if(t->quantity == x.second->quantity){
+                            sell_queues[s].pop();
+                            //print the message
+                            std::cout<<t->broker<<" purchased "<<t->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
+                            }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << x.second->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            break;
+                        }
+                        else if(t->quantity > x.second->quantity){
+                            //print the message
+                            std::cout<<t->broker<<" purchased "<<x.second->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
+                            }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << x.second->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            t->quantity -= x.second->quantity;
                             sell_queues[s].pop();
                         }
-                        else if(min_time < sell_queues[s].top().second->start_time){
-                            sell_queues[s].pop();
-                        }
                     }
-                    if(t->quantity < x.second->quantity){
-                        x.second->quantity -= t->quantity;
-                    }
-                    if(t->quantity == x.second->quantity){
-                        for(int i=0;i<v.size();i++){
-                            if(v[i] == x){
-                                v.erase(v.begin()+i);
-                            }
-                        }
-                        for(int i=0;i<v.size();i++){
-                            sell_queues[s].push(v.back());
-                            v.pop_back();
-                        }
-                    }
-                    if(t->quantity > x.second->quantity){
-                        t->quantity -= x.second->quantity;
-                        for(int i=0;i<v.size();i++){
-                            if(v[i] == x){
-                                v.erase(v.begin()+i);
-                            }
-                        }
-                        for(int i=0;i<v.size();i++){
-                            sell_queues[s].push(v.back());
-                            v.pop_back();
-                        }
-                        buy_queues[s].push(std::make_pair(t->price,t));
-                    }
+                }
+
+                if(t->quantity > 0){
+                    buy_queues[s].push(std::make_pair(t->price,t));
+                }
+                
+                for(int i=0;i<v.size();i++){
+                    sell_queues[s].push(v.back());
+                    v.pop_back();
                 }
             }
             
         }
-        else{    // buy order
-            if(t->price > buy_queues[s].top().first){
+        else{    // sell order
+            if(buy_queues[s].empty()){
                 sell_queues[s].push(std::make_pair(t->price,t));
                 continue;
             }
-            if(t->price <= sell_queues[s].top().first){
-                while(!buy_queues[s].empty() && ((t->start_time > buy_queues[s].top().second->end_time && t->end_time > buy_queues[s].top().second->end_time) || (t->start_time < buy_queues[s].top().second->start_time && t->end_time < buy_queues[s].top().second->start_time))){
-                    // iterate through the sell_queues till we get a valid order
-                    v.push_back(buy_queues[s].top());
-                    buy_queues[s].pop();
-                }
-                if(buy_queues[s].empty()){  // all orders are invalid with current order
-                    while(!v.empty()){
-                        buy_queues[s].push(v.back());
-                        v.pop_back();
+            else{
+                while(!buy_queues.empty()){
+                    if(t->price > buy_queues[s].top().first){
+                        break;
                     }
-                    continue;
-                }
-                int p = buy_queues[s].top().first;
-                x = buy_queues[s].top();
-                int min_time = buy_queues[s].top().second->start_time;
-                while(!buy_queues.empty() && buy_queues[s].top().first == p){
-                    if(min_time > buy_queues[s].top().second->start_time){
-                        min_time = buy_queues[s].top().second->start_time;
+                    else if(t->price <= buy_queues[s].top().first){
+                        if(t->start_time > buy_queues[s].top().second->end_time){
+                            v.push_back(buy_queues[s].top());
+                            buy_queues[s].pop();
+                            continue;
+                        }
+                        // if it reaches here then it means we have found the deal for the best trade...now take it and go to next one if quantity still remains
                         x = buy_queues[s].top();
-                        buy_queues[s].pop();
-                        v.push_back(x);
-                    }
-                    if(min_time == buy_queues[s].top().second->start_time){
-                        if(x.second->broker > buy_queues[s].top().second->broker){
-                            x = buy_queues[s].top();
+                        if(t->quantity < x.second->quantity){
+                            x.second->quantity -= t->quantity;
+                            //print messsage
+                            std::cout<<x.second->broker<<" purchased "<<t->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
+                            }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << t->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            break;
                         }
-                        buy_queues[s].pop();
-                    }
-                    if(min_time < buy_queues[s].top().second->start_time){
-                        buy_queues[s].pop();
+                        else if(t->quantity == x.second->quantity){
+                            buy_queues[s].pop();
+                            //print the message
+                            std::cout<<x.second->broker<<" purchased "<<t->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
+                            }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << t->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            break;
+                        }
+                        else if(t->quantity > x.second->quantity){
+                            //print the message
+                            std::cout<<x.second->broker<<" purchased "<<x.second->quantity<<" share of ";
+                            if(t->companies.size() == 1){
+                                if(t->quantities[t->companies[0]] == 1){
+                                    std::cout << t->companies[0];
+                                }
+                                else{
+                                    std::cout << t->companies[0] << " " << t->quantities[t->companies[0]];
+                                }
+                            }
+                            else{
+                                for(int i=0;i<t->companies.size();i++){
+                                    std::cout << t->companies[i] << " " << t->quantities[t->companies[i]];
+                                }
+                            }
+                            std :: cout << " from " << t->broker << " for $" << x.first << "/share"<<std::endl;
+
+                            t->quantity -= x.second->quantity;
+                            buy_queues[s].pop();
+                        }
                     }
                 }
-                if(t->quantity < x.second->quantity){
-                    x.second->quantity -= t->quantity;
-                }
-                if(t->quantity == x.second->quantity){
-                    for(int i=0;i<v.size();i++){
-                        if(v[i] == x){
-                            v.erase(v.begin()+i);
-                        }
-                    }
-                    for(int i=0;i<v.size();i++){
-                        buy_queues[s].push(v.back());
-                        v.pop_back();
-                    }
-                }
-                if(t->quantity > x.second->quantity){
-                    t->quantity -= x.second->quantity;
-                    for(int i=0;i<v.size();i++){
-                        if(v[i] == x){
-                            v.erase(v.begin()+i);
-                        }
-                    }
-                    for(int i=0;i<v.size();i++){
-                        buy_queues[s].push(v.back());
-                        v.pop_back();
-                    }
+
+                if(t->quantity > 0){
                     sell_queues[s].push(std::make_pair(t->price,t));
                 }
+
+                for(int i=0;i<v.size();i++){
+                    buy_queues[s].push(v.back());
+                    v.pop_back();
+                }
+                
             }
+            
         }
     }
     return;
